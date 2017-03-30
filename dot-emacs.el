cd4267c9b5ddd603f2770ff4c3d2833b428b67be
Alexander Zinchenko <zinchenko@live.com>
2017-03-30 16:52:46 -0700

    Toggle window split

--- dot-emacs.el
+++ dot-emacs.el
@@ -153,6 +153,39 @@
 ;; TODO: There are a few good examples here: https://www.emacswiki.org/emacs/SqlMode
 ;; but i want to keep it vanila as for now
 
+;;-----------------------------------------------------
+;;--          Toggle Window Split                    --
+;;-----------------------------------------------------
+
+;; This implementation is coming from here:
+;; https://www.emacswiki.org/emacs/ToggleWindowSplit
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+;; Using C-x | to toogle window direction.
+(global-set-key (kbd "C-x |") 'toggle-window-split)
 
 ;;-----------------------------------------------------
 ;;--                                                 --
