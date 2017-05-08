5ee8668e031c6d0a6142a82e389a85cdc156024a
Alexander Zinchenko <zinchenko@live.com>
2017-05-08 14:44:23 -0700

    Proper fix for toolbar and scrool bar for both nox and gui configurations

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -329,7 +329,7 @@
 ;;----------------------------------------------------------------------------;;
 
 (menu-bar-mode -1)
-(if (display-graphic-p)
+(if window-system
     (progn
       (tool-bar-mode -1)
       (scroll-bar-mode -1)))
