995963fe9fec4a46af6e497502bd6b63ef34a59a
Oleksandr Zinchenko <zinchenko@live.com>
2017-11-30 16:34:18 -0800

    helm-swoop

--- packages/helm.el
+++ packages/helm.el
@@ -49,4 +49,26 @@
   (require 'helm-config)
   (helm-mode))
 
+;;
+(use-package helm-swoop
+  :ensure t
+  :demand
+
+  :bind (("M-i" . helm-swoop)
+         ("M-I" . helm-swoop-back-to-last-point)
+         ("C-c M-i" . helm-multi-swoop)
+         ("C-x M-i" . helm-multi-swoop-all)
+         :map isearch-mode-map
+         ("M-i" . helm-swoop-from-isearch)
+         :map helm-swoop-map
+         ("C-r" . helm-previous-line)
+         ("C-s" . helm-next-line)
+         :map helm-multi-swoop-map
+         ("C-r" . helm-previous-line)
+         ("C-s" . helm-next-line))
+  
+;;  :init
+;;  (setq helm-swoop-use-fuzzy-match t)
+  )
+
 ;;; helm.el ends here
