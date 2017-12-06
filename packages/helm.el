e5dd85605a953cdb8968da68ccfa35ebbfecd17c
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-06 12:02:36 -0800

    Cleaning up helm bindings

--- packages/helm.el
+++ packages/helm.el
@@ -17,13 +17,41 @@
 
   :init
   (require 'helm-config)
+
   (unbind-key "C-x c")
+  (unbind-key "F" helm-command-map) ;; helm-select-xfont
+  (unbind-key "/" helm-command-map) ;; helm-find
+  (unbind-key "8" helm-command-map) ;; helm-ucs
+  (unbind-key "@" helm-command-map) ;; helm-list-elisp-packages
+  (unbind-key "a" helm-command-map) ;; helm-apropos
+  (unbind-key "b" helm-command-map) ;; helm-resume
+  (unbind-key "c" helm-command-map) ;; helm-colors
+  (unbind-key "e" helm-command-map) ;; helm-etags-select
+  ;;(unbind-key "f" helm-command-map) ;; helm-milti-files
+  (unbind-key "l" helm-command-map) ;; helm-locate
+  (unbind-key "m" helm-command-map) ;; helm-man-woman
+  (unbind-key "p" helm-command-map) ;; helm-list-emacs-process
+  (unbind-key "r" helm-command-map) ;; helm-regexp
+  (unbind-key "s" helm-command-map) ;; helm-surfraw
+  (unbind-key "t" helm-command-map) ;; helm-top
+  (unbind-key "C-," helm-command-map) ;; helm-calcul-expression
+  (unbind-key "C-:" helm-command-map) ;; helm-eval-expression
+  (unbind-key "M-x" helm-command-map) ;; helm-M-x
+  (unbind-key "M-y" helm-command-map) ;; helm-show-kill-ring
+  (unbind-key "<tab>" helm-command-map) ;; helm-lisp-completion-at-point
+  (unbind-key "C-c" helm-command-map)
+  (unbind-key "C-x" helm-command-map)
+  (unbind-key "M-g" helm-command-map)
+  (unbind-key "M-s" helm-command-map)
+  (unbind-key "h" helm-command-map)
+
   (setq helm-M-x-fuzzy-match t
         helm-apropos-fuzzy-match t
         helm-buffers-fuzzy-matching t
         helm-ff-newfile-prompt-p nil
         helm-locate-fuzzy-match t
         helm-recentf-fuzzy-match t)
+
   (helm-mode))
 
 (use-package helm-swoop
