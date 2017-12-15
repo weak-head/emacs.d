d1b294afc089d435b461f1554eb8c6baf7b42998
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-15 11:47:09 -0800

    fancy narrow: basic key bindings

--- packages/demo.el
+++ packages/demo.el
@@ -1,12 +1,18 @@
 (use-package demo-it
-  :ensure f
+  :ensure t
   :defer t)
 
 (use-package expand-region
-  :ensure f
+  :ensure t
   :defer t
   :bind ("C-=" . er/expand-region))
 
 (use-package fancy-narrow
-  :ensure f
+  :ensure t
+
+  :bind (("C-c 0 r" . fancy-narrow-to-region)
+         ("C-c 0 d" . fancy-narrow-to-defun)
+         ("C-c 0 p" . fancy-narrow-to-page)
+         ("C-c 0 w" . fancy-widen))
+
   :defer t)
