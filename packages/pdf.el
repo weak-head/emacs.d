8a47511677b2676ab67f51ec72f2dbf3fd05027b
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-13 11:09:29 -0800

    auto loading pdf-tools for *.pdf files

--- packages/pdf.el
+++ packages/pdf.el
@@ -1,4 +1,12 @@
+
+;; Postconfigure:
+;;  - (pdf-tools-install)
+;;
+
 (use-package pdf-tools
   :ensure t
-  :demand
-  :diminish "")
+  :defer t
+
+  :init
+  (pdf-tools-install)
+  (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode)))
