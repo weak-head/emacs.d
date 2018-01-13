471fc9b621a74f677c00817e9df80d9b08206136
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-12 20:57:49 -0800

    Markdown: a little bit of something

--- packages/markdown.el
+++ packages/markdown.el
@@ -1,3 +1,20 @@
+
+;; Prerequisites:
+;;   - pandoc
+
 (use-package markdown-mode
   :ensure t
-  :defer t)
+  :defer t
+
+  :commands (markdown-mode gfm-mode)
+
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+
+  :init (setq markdown-command "pandoc")
+
+  :config
+  (add-hook 'gfm-mode-hook
+            (lambda () (visual-line-mode 1)))
+  )
