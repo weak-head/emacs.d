63c132214b10601fa113c77d696b1f93bf34b15f
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-16 14:34:00 -0800

    neotree defun as part of use-package definition

--- packages/neotree.el
+++ packages/neotree.el
@@ -4,24 +4,25 @@
 (use-package neotree
   :ensure t
 
-  :bind  ([f8] . neotree-project-dir)
+  :bind  ([f8] . wh/neotree-project-dir)
 
   :init
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   ;;(setq neo-window-fixed-size 'nil)
-  (setq neo-window-width 40))
+  (setq neo-window-width 40)
 
-(defun neotree-project-dir ()
-  "Open NeoTree using projectile root or toggle neotree if outside of a project."
-  (interactive)
-  (condition-case nil
-      (let ((project-dir (projectile-project-root))
-            (file-name (buffer-file-name)))
-        (neotree-toggle)
-        (if project-dir
-            (if (neo-global--window-exists-p)
-                (progn
-                  (neotree-dir project-dir)
-                  (neotree-find file-name)))
-          (message "Could not find project root.")))
+  (defun wh/neotree-project-dir ()
+    "Open NeoTree using projectile root or toggle neotree if outside of a project."
+    (interactive)
+    (condition-case nil
+        (let ((project-dir (projectile-project-root))
+              (file-name (buffer-file-name)))
+          (neotree-toggle)
+          (if project-dir
+              (if (neo-global--window-exists-p)
+                  (progn
+                    (neotree-dir project-dir)
+                    (neotree-find file-name)))
+            (message "Could not find project root.")))
       (error (neotree-toggle))))
+  )
