86ebb75a579ae3f0a72c2c4666dff9d7673515ab
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-16 18:46:55 -0800

    dashboard skeleton

---
+++ packages/dashboard.el
@@ -0,0 +1,26 @@
+;; Prerequisites:
+;;  - [Windows specific]: https://ftp.wayne.edu/gnu/emacs/windows/
+;;    >> emacs-25.3_1-x86_64.zip
+;;    >> emacs-25-x86_64-deps.zip
+
+(use-package dashboard
+  :ensure t
+
+  :init
+  (defun wh/emacs-logo-title ()
+    "Generates the emacs logo title."
+    (cond ((eq system-type 'windows-nt) (concat "GNU Emacs: " emacs-version))
+          ((eq system-type 'darwin) (concat "GNU Emacs: " emacs-version))
+          (t (concat "GNU Emacs: " emacs-version
+                     "; kernel: " (car (split-string (shell-command-to-string "uname -r") "-"))
+                     "; os: " (nth 1 (split-string (car (split-string (shell-command-to-string "cat /etc/*-release") "\n")) "="))))))
+
+  (setq dashboard-banner-logo-title (wh/emacs-logo-title))
+  (setq dashboard-startup-banner 'logo)
+
+  (setq dashboard-items '((recents   . 5)
+                          (bookmarks . 5)
+                          (projects . 5)))
+
+  :config
+  (dashboard-setup-startup-hook))
