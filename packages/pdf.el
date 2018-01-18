25e967986b9ca5c23d3898a8b560d30eb2e66b09
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-17 20:31:55 -0800

    installing pdf-tools without a confirmation

--- packages/pdf.el
+++ packages/pdf.el
@@ -1,14 +1,10 @@
 
-;; Postconfigure:
-;;  - (pdf-tools-install)
-;;
-
 (use-package pdf-tools
   :ensure t
   :defer t
 
   :mode ("\\.pdf\\'" . pdf-view-mode)
 
-  ;:init
-  ;;(pdf-tools-install)
+  :init
+  (pdf-tools-install 't)
   )
