02b455b575cece6ea900ed49b26572a84ac63153
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-17 22:49:52 -0800

    javascript configuration skeleton

---
+++ packages/javascript.el
@@ -0,0 +1,10 @@
+
+;; Prerequisites:
+;;  - NodeJS
+;;    $ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+;;    $ nvm install v8
+;;    $ nvm alias default v8
+
+(use-package indium
+  :ensure t
+  )
