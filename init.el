2d72994d8d8ce85aba4d05090d4a408b6f3edf4f
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-30 16:00:51 -0800

    Moving utf-8 encoding setup to the init file.
    Because of some Unicode characters in the configuration, we have a nasty prompt on Windows system
    that we want to avoid.

--- init.el
+++ init.el
@@ -24,6 +24,15 @@
 
 
 ;;----------------------------------------------------------------------------
+;; Default encoding
+;;----------------------------------------------------------------------------
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+
+;;----------------------------------------------------------------------------
 ;; Initialize use-package
 ;;----------------------------------------------------------------------------
 (unless (package-installed-p 'use-package)
