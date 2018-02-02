556557b58d0c57aa9813b49068418c51154f0b8f
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-02 15:43:00 -0800

    Using external 'minimap' fork from github, instead of MELPA.
    The official minimap is not well maintained and has some issues. Instead of original minimap I'm
    using fork.

--- packages/minimap.el
+++ packages/minimap.el
@@ -1,10 +1,21 @@
+
+;; The original minimap is not well-maintained any more.
+;; I've grabbed the one from the `https://github.com/br0ns/minimap' repo.
+;; It has a fix for the highlight line, ability to customize the line face and
+;; a way to hide the mode line.
 (use-package minimap
-  :ensure t
+  :load-path "external-modules"
   :diminish "mm"
 
   :bind ("C-c t m" . minimap-mode)
 
   :config
+  ;; (setq minimap-highlight-line nil)
+  ;; (setq minimap-show-mode-line nil)
+
+  (set-face-attribute 'minimap-highlight-line-face nil
+                      :background "#a9a1e1"
+                      :foreground "#a9a1e1") ;; dark gray
   (set-face-attribute 'minimap-active-region-background nil
-                      :background "light slate gray")
+                      :background "#5B6268") ;; light slate gray
   )
