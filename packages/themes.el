0e090a178ad7371b728334795fbceb0e336d1c8d
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-04-10 10:04:49 -0700

    telephone line

--- packages/themes.el
+++ packages/themes.el
@@ -93,9 +93,24 @@
 ;; Setup power line
 ;;----------------------------------------------------------------------------
 
+(use-package telephone-line
+  :ensure t
+  :demand
+
+  :init
+  (setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+        telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+        telephone-line-primary-right-separator 'telephone-line-cubed-right
+        telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+  (setq telephone-line-height 24
+        telephone-line-evil-use-short-tag t)
+
+  (telephone-line-mode 1))
+
 (use-package spaceline
   :ensure t
   :demand
+  :disabled t
 
   :init
   (require 'spaceline-config)
